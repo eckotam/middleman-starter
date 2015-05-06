8e9a052e76c1fcb56f27ff1e562ed9712d153e67
Ecko Tam <eckotam@gmail.com>
2015-05-06 15:44:50 +1000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+# If you do not have OpenSSL installed, update
+# the following line to use "http://" instead
+source 'https://rubygems.org'
+
+gem "middleman", "~>3.3.11"
+
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
+# Windows does not come with time zone data
+gem "tzinfo-data", platforms: [:mswin, :mingw]
+
+# Compress images on build
+gem 'image_optim', '~> 0.18.0'
+gem 'image_optim_pack'
+
+# Deploy to dedicated FTP - https://github.com/karlfreeman/middleman-deploy
+gem 'middleman-deploy', '~> 1.0'
+
+# Prettify HTML after middleman build
+gem "htmlbeautifier"
---
+++ config.rb
@@ -0,0 +1,70 @@
+# Directory Setup
+set :css_dir, 'assets/css'
+set :sass_dir, 'assets/css'
+set :js_dir, 'assets/js'
+set :images_dir, 'assets/img'
+set :fonts_dir, 'assets/fonts'
+set :partials_dir, 'partials'
+set :sass, {
+	sourcemap: false
+}
+
+# Configure relative links
+set :relative_links, true
+
+# Set HAML as template language
+set :haml, { :ugly => true, :format => :html5 }
+
+# Bower Configuration
+after_configuration do
+	@bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+	sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
+# Import packages from bower
+sprockets.import_asset 'foundation'
+sprockets.import_asset 'bourbon'
+
+# Development-specific configuration
+configure :development do
+	# Debug assets to true when in development
+	set :debug_assets, true
+
+	# Pretty URLs
+	activate :directory_indexes
+
+	# Reload the browser automatically whenever files change
+	activate :livereload
+end
+
+# Build-specific configuration
+configure :build do
+	# For example, change the Compass output style for deployment
+	activate :minify_css
+
+	# Minify Javascript on build
+	activate :minify_javascript
+
+	# Enable cache buster
+	# activate :asset_hash
+
+	# Use relative URLs
+	activate :relative_assets
+
+	# Pretty URLs
+	activate :directory_indexes
+
+	after_build do
+		system('htmlbeautifier build/*.html --tab-stops 4')
+	end
+end
+
+activate :deploy do |deploy|
+	deploy.method   = :ftp
+	deploy.host     = 'FTP_HOST'
+	deploy.path     = '/public_html/PROJECT_NAME'
+	# Optional Settings
+	deploy.user     = '' # no default
+	deploy.password = '' # no default
+	deploy.build_before = true # default: false
+end
